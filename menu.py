1dbbcc07553dba4d97bbb37135390216bf049393
Hui Yuk Chun <cyberishthingy@gmail.com>
2026-04-02 02:00:36 +0800

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,99 +1,90 @@
-# I will continue this on Thursday cos I realised that I have an urgent essay to do
-
-#TODO: import ezeikel code
-from sys import exit
+# 2/4/2026 2am update:
+# Grouped menu functionality together into the menu class
+# Removed data storage functionality of the 
+# I might split off the hard coded implementations of menus (e.g. the main menu) into another file
 PREFERENCE_MODES = ["Distance", "Cost", "Segments"]
 TRANSPORT_MODES = ["MTR", "Bus", "Walking"]
+MAIN_MENU_OPTIONS = ["Upload Map", "Plan Route", "Change Settings", "Help", "Exit"]
 
 def main():
-    pass
+    MAIN_MENU_OPTIONS_DICT = {x:print for x in MAIN_MENU_OPTIONS}
+    main_menu = MenuPage(MAIN_MENU_OPTIONS_DICT, "Welcome to transport advisor.")
+    main_menu.mainloop()
 
-# This class is the main menu and stores stuff
+# This class is for menu pages where the user is prompted to choose from several options
 class MenuPage:
-    def __init__(self):
-        self.network = None # network object
-        self.options = {
-            "avoid_modes" : [],
-            "max_budget": 0,
-            "max_segments": 0,
-            "preferred_modes": []
-        } # this is a dictionary
+    # valid_options is inteded to be a dict of string : function
+    def __init__(self, valid_options: dict, user_prompt: str ="",
+                        multiple: bool =False, delimiter: str =" ",
+                        case_sensitive: bool=False):
+        
+        self.valid_options = valid_options
+        self.user_prompt = user_prompt
+        self.multiple = multiple
+        self.delimiter = delimiter
+        self.case_sensitive = case_sensitive
+        
+        # This list is for direct comparison in validation, entirely lowercase if not case sensitive
+        self.checked_options = self.valid_options.keys() if self.case_sensitive \
+        else [option.lower() for option in self.valid_options]
 
+    # Loop menu indefinitely, include options like "return"/"exit"?
     def mainloop(self):
-        MAIN_MENU_OPTIONS = ["Upload Map", "Plan Route", "Change Settings", "Help", "Exit"]
-        USER_PROMPT = "Welcome to transport advisor."
-
         while True:
-            interface = prompt_input_variable(USER_PROMPT, MAIN_MENU_OPTIONS)
+            self.prompt_options()
+            user_choice = self.get_user_input()
 
-            match interface.lower():
-
+            # TODO: split this off and not hardcode the options
+            match user_choice.lower():
                 case "upload map":
-                    self.upload_network()
+                    print("TODO: UPLOAD MAP")
                 
                 case "plan route":
-                    pass # carl code
+                    print("TODO: PLAN ROUTE")
 
                 case "change settings":
-                    self.change_setting()
+                    print("TODO: CHANGE SETTINGS")
                 
                 case "help":
-                    self.print_help()
+                    print("TODO: HELP")
 
-    def change_setting(self):
-        pass
-
-    def print_help(self):
-        print("placeholder TODO: CHANGE THIS")
-
-    def upload_network(self):
-        pass
-
-    def exit(self):
-        print("Exiting program.")
-        exit(0)
-
-# Gets user to input variable value and return it
-def prompt_input_variable(user_prompt: str, valid_options: list,
-                          multiple: bool =False, delimiter: str =" ",
-                          case_sensitive: bool=False, input_prompt : str=">>> "):
-    
-    # Prompt the user for input
-    print(user_prompt)
-
-    if multiple:
-        print(f"Input one or more options delimited by \"{delimiter}\".")
-    
-    if case_sensitive:
-        print("Valid options include (case sensitive):")
-    else:
-        print("Valid options include:")
-
-    for option in valid_options:
-        print(f"->{option}")
-
-    # Get user input and validate it
-    is_valid = False
-    
-    # If not case_sensitive, set all valid options to lowercase
-    if not case_sensitive:
-        valid_options = [option.lower() for option in valid_options]
-
-    while not is_valid:
+    # Gets user to input variable value and return it
+    def prompt_options(self):
         
-        user_input = input(input_prompt)
-
-        # Validate the input
-        if not multiple:
-            is_valid = user_input in valid_options if case_sensitive else user_input.lower() in valid_options
-        else:
-            user_input = user_input.split(delimiter)
-            if case_sensitive:
-                is_valid = all([item in valid_options for item in user_input])
-            else:
-                is_valid = all([item.lower() in valid_options for item in user_input])
+        # Prompt the user for input (if any)
+        if self.user_prompt:
+            print(self.user_prompt)
     
-    # Return value: string if not multiple, list of strings if multiple
-    return user_input
+        if self.multiple:
+            print(f"Input one or more options delimited by \"{self.delimiter}\".")
+        
+        if self.case_sensitive:
+            print("Valid options include (case sensitive):")
+        else:
+            print("Valid options include:")
+    
+        for option in self.valid_options.keys():
+            print(f"-> {option}")
+    
+    # Repeatedly read user input until it is valid
+    def get_user_input(self):
+        is_valid = False
 
-main()
+        while not is_valid:
+            
+            user_input = input(">>> ")
+
+            user_input = user_input if self.case_sensitive else user_input.lower()
+    
+            # Validate the input
+            if not self.multiple:
+                is_valid = user_input in self.checked_options
+            else:
+                user_input = user_input.split(self.delimiter)
+                is_valid = all([item in self.checked_options for item in user_input])
+        
+        # Return value: string if not multiple, list of strings if multiple
+        return user_input
+
+if __name__ == "__main__":
+    main()
