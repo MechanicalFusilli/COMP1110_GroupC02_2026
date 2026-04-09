b132739036ce6b45b8884097ec40190645a63282
Hui Yuk Chun <cyberishthingy@gmail.com>
2026-04-09 17:42:54 +0800

    Add files via upload

--- menu_instances.py
+++ menu_instances.py
@@ -1,82 +1,67 @@
 # Implementation of the menus used
-# TO MY TEAMMATES: focus on the functions build_all_menus, build_main_menu and build_settings_menu
-# (yes this is hardcoding im sorry)
+# (yes this is hardcoding I'm sorry)
 from menu import MenuPage
 from sys import exit
 
 # This file is just for buildling the menus, I will likely remove this main, this is for testing
 def main():
-    main_menu = build_all_menus()
-    main_menu.mainloop()
+    MenuSystem()
 
-# Builds all menus, returns only the main menu
-def build_all_menus():
-    settings_menu = build_settings_menu()
-    main_menu = build_main_menu(settings_menu) # Idk what I'm doing with the function scope anymore
-    return main_menu
+# Effectively using classes as an excuse to have global variables
+# All of the menus in this
+class MenuSystem:
+    def __init__(self, network_system):
+        # network system is inteded to contain information about the network
+        # and it also makes the adjacency list that Carl's djikstra needs
+        # this is the main thing that I will be doing later
+        self.network_system = network_system
+        self.build_all_menus()
+        self.main_menu.mainloop()
 
-def build_main_menu(settings_menu):
-    # Function implementations of various functions go in options_dict
-    options_dict = {
-        "Upload Map" : None, # your implementations go here, replace the "None"
-        "Plan Route" : None,
-        "Change Settings" : settings_menu.mainloop, # enter settings menu
-        "Help" : print_help_main,
-        "Quit" : quit_program
-    }
+    def build_all_menus(self):
+        self.build_settings_menu()
+        self.build_main_menu()
 
-    main_menu = MenuPage(
-        options_dict,
-        user_prompt="~~ Welcome to transport advisor ~~",
-        has_return=False
-    )
-
-    return main_menu
-
-# I still have not decided what to print here
-def print_help_main():
-    print("TODO: IMPLEMENT INSTRUCTIONS IN HELP")
-    print("This transport advisor helps you find the best route given certain conditions (which can be adjusted in settings).")
-    print("~~ Main Menu Options ~~")
-    print("Upload Map: TODO EXPLANATION")
-    print("Plan Route: TODO EXPLANATION")
-    print("Change Settings: There is a help page within the settings menu that will explain the settings.")
-    print("Help: Print this page.")
-    print("Quit: Exit the program.")
-    print()
-
-def quit_program():
-    print("~~ See you next time ~~")
-    exit(0)
-
-def build_settings_menu():
-    options_dict = {
-        # ref to Ezekiel's part: preferences are cheapest, fastest, fewest
-        "Set Preference" : None,
-        "Set Avoid Modes" : None,
-        "Set Preferred Modes" : None,
-        "Set Max Budget" : None,
-        "Set Max Segments" : None,
-        "Help": print_help_settings,
-    }
+    def build_main_menu(self):
+        # Function implementations of various functions go in options_dict
+        options_dict = {
+            "Upload Map" : None, # I will hook up the implementations here later
+            "Plan Route" : None,
+            "Change Settings" : self.settings_menu.mainloop, # enter settings menu
+            "Help" : lambda: self.print_help("main_menu_help.txt"),
+            "Quit" : self.quit_program
+        }
     
-    settings_menu = MenuPage(
-        options_dict,
-        user_prompt="~~ Settings ~~",
-    )
+        self.main_menu = MenuPage(
+            options_dict,
+            user_prompt="~~ Welcome to transport advisor ~~",
+            has_return=False
+        )
+    
+    def build_settings_menu(self):
+        options_dict = {
+            # ref to Carl's part: preferences are cheapest, fastest, fewest
+            # I'll do this later
+            "Set Preference" : None, # This should be a number
+            "Set Avoid Modes" : None, # This should be a list of banned modes
+            "Help": lambda: self.print_help("settings_menu_help.txt"),
+        }
+        
+        self.settings_menu = MenuPage(
+            options_dict,
+            user_prompt="~~ Settings ~~",
+        )
+    
+    # Print whatever help page is there
+    def print_help(self, filepath):
+        with open(filepath, "r") as file:
+            help_content = file.read()
+        
+        print(help_content)
 
-    return settings_menu
-
-def print_help_settings():
-    print("~~ Options in Settings ~~")
-    print("Preference: choose whether the best route is determined by being the cheapest, fastest or having the fewest segments.")
-    print("Set Avoid Modes: Set which modes of transport to avoid.")
-    print("Set Preferred Modes: Set which modes of transport to prioritise.")
-    print("Set Max Budget: Set the maximum price for a valid route.")
-    print("Set Max Segments: Set the maximum number of segments for a valid route.")
-    print("Help: Print this page.")
-    print("Return: Returns to the main menu.")
-    print()
+    def quit_program():
+        print("~~ See you next time ~~")
+        exit(0)
 
 if __name__ == '__main__':
     main()
