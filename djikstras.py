8edfac0d15470248fbe24c514582b61b21a5a013
Ez <coezekiel868@gmail.com>
2026-05-01 20:47:09 +0800

    revert change

--- djikstras.py
+++ djikstras.py
@@ -92,7 +92,7 @@ def yens(start, end, e):
             if w == -1:
                 continue
 
-            heapq.heappush(potential, [w + rootw, root + r[1:]])
+            heapq.heappush(potential, [w + rootw, root[:-1] + r])
 
         if potential:
             dist, new_path = heapq.heappop(potential)
