2949ee97ee7cd221474e96ae95ad80fa5f54859b
Ez <coezekiel868@gmail.com>
2026-05-01 20:46:30 +0800

    added numbers

--- menu.py
+++ menu.py
@@ -20,7 +20,6 @@ def main():
 
 # This class is for menu pages where the user is prompted to choose from several options
 class MenuPage:
-    # valid_options is inteded to be a dict of string : function
     def __init__(self, valid_options: dict, user_prompt: str ="",
                         multiple: bool =False, delimiter: str ="/",
                         case_sensitive: bool=False, has_return: bool=True,
@@ -32,96 +31,104 @@ class MenuPage:
         self.delimiter = delimiter
         self.case_sensitive = case_sensitive
         self.auto_return = auto_return
-        
-        # has_return: there exists a "Return option that breaks out of the mainloop"
+
         if has_return:
             self.valid_options["Return"] = None
-        
-        # These are the options that are printed
-        self.visual_options = self.valid_options.keys()
-        
-        # Set keys of valid options to lowercase if not case_sensitive
+
+        self.visual_options = list(self.valid_options.keys())
+
+        # Store number choices
+        self.number_map = {
+            str(i + 1): label for i, label in enumerate(self.visual_options)
+        }
+
         if not case_sensitive:
-            self.valid_options = {label.lower():func for label, func in self.valid_options.items()}
+            self.valid_options = {
+                label.lower(): func for label, func in self.valid_options.items()
+            }
 
-    # Loop menu indefinitely, include options like "return"/"exit"?
     def mainloop(self):
-
         while True:
-            # Prompt and read user input
             self.prompt_options()
             user_choice = self.get_user_input()
-            
-            # Break the loop immediately if return is in any part of user input
+
             if self.multiple:
                 if "return" in [choice.lower() for choice in user_choice]:
-                    print("\033[33m" + "Returning to previous page..." + "\033[0m")
-                    print("\033[33m" + "(If other options were selected, they will be disregarded)" + "\033[0m")
+                    print("\033[33mReturning to previous page...\033[0m")
                     break
-            elif user_choice.lower() == "return":
-                print("\033[33m" + "Returning to previous page..." + "\033[0m")
-                break
+            else:
+                if user_choice.lower() == "return":
+                    print("\033[33mReturning to previous page...\033[0m")
+                    break
 
-            # Execute function(s) associated with choice
-            # https://stackoverflow.com/questions/624926/how-do-i-detect-whether-a-variable-is-a-function
             if not self.multiple:
                 self.run_function(user_choice)
             else:
                 for choice in user_choice:
                     self.run_function(choice)
-            
+
             if self.auto_return:
-                print("\033[33m" + "Returning to previous page..." + "\033[0m")
+                print("\033[33mReturning to previous page...\033[0m")
                 break
-    
-    # Get and run functions given a label
+
     def run_function(self, label):
         function = self.valid_options.get(label)
+
         if callable(function):
             function()
 
-    # Gets user to input variable value and return it
     def prompt_options(self):
-        
-        # Prompt the user for input (if any)
         if self.user_prompt:
             print(self.user_prompt)
-    
+
         if self.multiple:
             print(f"Input one or more options delimited by \"{self.delimiter}\".")
-        
-        if self.case_sensitive:
-            print("Valid options include (case sensitive):")
-        else:
-            print("Valid options include:")
-    
-        for option in self.visual_options:
-            print(f"-> {option}")
-    
-    # Repeatedly read user input until it is valid
+
+        print("Choose an option:")
+
+        for i, option in enumerate(self.visual_options, 1):
+            print(f"({i}) {option}")
+
     def get_user_input(self):
-        is_valid = False
+        while True:
+            user_input = input(">>> ").strip()
 
-        while not is_valid:
-            
-            user_input = input(">>> ")
-
-            user_input = user_input if self.case_sensitive else user_input.lower()
-    
-            # Validate the input
             if not self.multiple:
-                is_valid = user_input in self.valid_options.keys()
-            else:
-                user_input = user_input.split(self.delimiter)
-                in_options = all([item in self.valid_options.keys() for item in user_input])
-                is_unique = (len(user_input) == len(set(user_input)))
-                is_valid = in_options and is_unique
-        
-        print()
+                if user_input in self.number_map:
+                    user_input = self.number_map[user_input]
 
-        # Return value: string if not multiple, list of strings if multiple
-        # The return value(s) will be in lowercase if not case_sensitive
-        return user_input
+                if not self.case_sensitive:
+                    user_input = user_input.lower()
+
+                if user_input in self.valid_options:
+                    print()
+                    return user_input
+
+            else:
+                parts = user_input.split(self.delimiter)
+                converted = []
+                valid = True
+
+                for part in parts:
+                    part = part.strip()
+
+                    if part in self.number_map:
+                        part = self.number_map[part]
+
+                    if not self.case_sensitive:
+                        part = part.lower()
+
+                    if part not in self.valid_options:
+                        valid = False
+                        break
+
+                    converted.append(part)
+
+                if valid and len(converted) == len(set(converted)):
+                    print()
+                    return converted
+
+            print("Invalid input. Please enter a valid number.")
 
 if __name__ == "__main__":
     main()
